9661f0e39be5ab78a525af0174376f86afc7554f
Amvilina <diatimp@gmail.com>
2024-10-28 03:22:25 +0300

    main.cpp now have examples and 'visual tests' of the testing system

--- UnitTestSystem/main.cpp
+++ UnitTestSystem/main.cpp
@@ -1,14 +1,91 @@
-//
-//  main.cpp
-//  UnitTestSystem
-//
-//  Created by Антон Творожков on 27.10.2024.
-//
+#include <thread>
+#include <vector>
+#include "UnitTestSystem.h"
 
-#include <iostream>
+TEST_MODULE_BEGIN(FirstModule)
+
+TEST_METHOD(CorrectCode) {
+    MUST_BE_TRUE(true);
+    MUST_BE_TRUE(1 + 1 > 0);
+    
+    MUST_BE_FALSE(false);
+    MUST_BE_FALSE(3 == 4);
+    
+    MUST_BE_CLOSE_DOUBLES(1.234567, 1 + 0.234566);
+    
+    MUST_THROW_EXCEPTION(throw 123);
+    
+    std::vector<int> vec;
+    MUST_THROW_SPECIFIC_EXCEPTION(std::out_of_range, vec.at(3));
+}
+
+TEST_METHOD(MUST_BE_TRUE_error) {
+    MUST_BE_TRUE(1 < 0);
+}
+
+TEST_METHOD(MUST_BE_FALSE_error) {
+    MUST_BE_FALSE(1 != 0);
+}
+
+TEST_METHOD(MUST_BE_CLOSE_DOUBLES_error) {
+    MUST_BE_CLOSE_DOUBLES(1.1, 1.0 + 0.01);
+}
+
+TEST_METHOD(MUST_THROW_EXCEPTION_error) {
+    MUST_THROW_EXCEPTION(1+1);
+}
+
+TEST_METHOD(MUST_THROW_SPECIFIC_EXCEPTION_error1) {
+    MUST_THROW_SPECIFIC_EXCEPTION(std::out_of_range, 1+1);
+}
+
+TEST_METHOD(MUST_THROW_SPECIFIC_EXCEPTION_error2) {
+    std::vector<int> vec;
+    MUST_THROW_SPECIFIC_EXCEPTION(std::bad_cast, vec.at(0));
+}
+
+TEST_METHOD(RandomException) {
+    throw 123;
+}
+
+TEST_METHOD(MemoryLeak) {
+    auto a = new char;
+}
+
+TEST_METHOD(NoMemoryLeak) {
+    auto a = new char;
+    delete a;
+    
+    auto arr = new int[13];
+    delete [] arr;
+}
+
+TEST_TIME_MEASURING_METHOD(Time) {
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(0.1s);
+}
+
+TEST_TIME_MEASURING_METHOD(TimeNoIfError) {
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(0.1s);
+    MUST_BE_TRUE(false);
+}
+
+TEST_TIME_MEASURING_METHOD(TimeNoIfMemoryLeak) {
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(0.1s);
+    auto a = new char;
+}
+
+TEST_MODULE_END
+
+
+TEST_MODULE_BEGIN(SecondEmptyModule)
+TEST_MODULE_END
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    FirstModule::Run();
+    SecondEmptyModule::Run();
     return 0;
 }
